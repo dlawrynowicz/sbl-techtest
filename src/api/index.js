e19e4acd02c2e2fab8dfdc2a6b90da5416fef659
d.lawrynowicz <fabrykasoftu@gmail.com>
2020-04-16 09:00:51 +0100

    Update api to return custom data structure

--- src/api/index.js
+++ src/api/index.js
@@ -1,3 +1,29 @@
 const endpointUrl = process.env.REACT_APP_API_URL;
 
-export const fetchCards = async () => fetch(endpointUrl);
+const defaultResponse = { data: [], error: null };
+const defaultErrorMessage = "Something went wrong";
+
+const errorResponse = (response, cb = () => defaultErrorMessage) => {
+  return Object.assign({}, defaultResponse, {
+    error: cb(response)
+  });
+};
+
+export const fetchCards = async () => {
+  const response = await fetch(endpointUrl);
+
+  // check http client response
+  if (response.status >= 300) {
+    return errorResponse(response);
+  }
+
+  const body = await response.json();
+
+  // check API response - seems like we have custom format to return status code even http client status was 200,
+  // double check to make sure - use returned message if avaiable
+  if (typeof body.status === "undefined" || body.status >= 300) {
+    return errorResponse(body, body => body.message || defaultErrorMessage);
+  }
+
+  return Object.assign({}, defaultResponse, { data: body.items || [] });
+};
